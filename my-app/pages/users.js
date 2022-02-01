cc3eddbc8818c47b4bf69289a1a76752898e29ba
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-02-01 12:14:26 +0300

    added async await

--- my-app/pages/users.js
+++ my-app/pages/users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 
 const Users = () => {
@@ -7,6 +7,13 @@ const Users = () => {
     { id: 2, name: 'Kolya' },
     { id: 3, name: 'Vasya' },
   ]);
+
+  useEffect(async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    setUsers(data);
+  }, []);
+
   return (
     <div>
       <h1>Users</h1>

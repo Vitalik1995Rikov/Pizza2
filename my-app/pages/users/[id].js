3e748e5d8f2e383ac6fa942697e53c63c9fd7603
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-06-02 19:48:12 +0300

    start project

--- my-app/pages/users/[id].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router';
-
-const User = () => {
-  const { query } = useRouter();
-
-  return <div>Пользователь c id {query.id}</div>;
-};
-
-export default User;
---
+++ my-app/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div>Pizza2</div>;
+};
+
+export default App;

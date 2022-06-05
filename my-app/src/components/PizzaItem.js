11fed350bc754343229814423bfce5a07ea9a890
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-06-05 18:25:25 +0300

    added pizzaTypes and logic for choose type

--- my-app/src/components/PizzaItem.js
+++ my-app/src/components/PizzaItem.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 
 function PizzaItem({ obj }) {
+  const typesOfPizzas = ['тонкое', 'стандарт'];
+  const [activeType, setActiveType] = useState('');
   return (
     <div>
       <li key={obj.id}>
@@ -9,8 +11,13 @@ function PizzaItem({ obj }) {
         <span>{obj.name}</span>
         <div className="bg-[#858383]">
           <ul className="grid grid-cols-2">
-            <li>тонкое</li>
-            <li>стандарт</li>
+            {obj.types.map((item, i) => (
+              <li
+                onClick={() => setActiveType(i)}
+                className={activeType === i ? 'bg-slate-50' : ''}>
+                {typesOfPizzas[i]}
+              </li>
+            ))}
           </ul>
           <ul className="grid grid-cols-3">
             {obj.sizes.map((item) => (

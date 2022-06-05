db29946160dfc4d66a0b495b46376b222e81a0a4
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-06-05 17:45:50 +0300

    added prices and table

--- my-app/src/components/PizzaItem.js
+++ my-app/src/components/PizzaItem.js
@@ -7,8 +7,22 @@ function PizzaItem({ obj }) {
       <li key={obj.id}>
         <img className="w-40" src={obj.img} alt="img" />
         <span>{obj.name}</span>
-        <div>
-          <Button />
+        <div className="bg-[#858383]">
+          <ul className="grid grid-cols-2">
+            <li>тонкое</li>
+            <li>стандарт</li>
+          </ul>
+          <ul className="grid grid-cols-3">
+            <li>25</li>
+            <li>30</li>
+            <li>35</li>
+          </ul>
+        </div>
+        <div className="flex">
+          <div>{obj.price}</div>
+          <div>
+            <Button />
+          </div>
         </div>
       </li>
     </div>

9c908a4c1eec889cb5b7822aed02ef7932fe1ee9
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-06-06 21:23:04 +0300

    added logic for sort

--- my-app/src/components/Sort.js
+++ my-app/src/components/Sort.js
@@ -1,5 +1,19 @@
+import { useState } from 'react';
+
 function Sort() {
-  return <div>Sort</div>;
+  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <div>
+      <div onClick={() => setIsVisible(!isVisible)}>Sort by:</div>
+      {isVisible && (
+        <div>
+          <div>Popular</div>
+          <div>Price</div>
+          <div>Alphabet</div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Sort;

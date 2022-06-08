1923707a3a4ec6c94abdb2ffbe44595fedabdf8d
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-06-08 22:43:20 +0300

    added logic for choose sort item

--- my-app/src/components/Sort.js
+++ my-app/src/components/Sort.js
@@ -2,14 +2,18 @@ import { useState } from 'react';
 
 function Sort() {
   const [isVisible, setIsVisible] = useState(false);
+  const [selected, setSelected] = useState(0);
+  const data = ['Popular', 'Price', 'Alphabet'];
   return (
     <div>
       <div onClick={() => setIsVisible(!isVisible)}>Sort by:</div>
       {isVisible && (
         <div>
-          <div>Popular</div>
-          <div>Price</div>
-          <div>Alphabet</div>
+          {data.map((item, i) => (
+            <div onClick={() => setSelected(i)} className={selected === i ? 'bg-slate-500' : ''}>
+              {item}
+            </div>
+          ))}
         </div>
       )}
     </div>

256c21cc32527533e5ce7c95b7e205670a50ad62
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-06-09 20:21:48 +0300

    added logic for choose sort and show it

--- my-app/src/components/Sort.js
+++ my-app/src/components/Sort.js
@@ -3,14 +3,18 @@ import { useState } from 'react';
 function Sort() {
   const [isVisible, setIsVisible] = useState(false);
   const [selected, setSelected] = useState(0);
+  const onClickSetItem = function (i) {
+    setSelected(i);
+    setIsVisible(false);
+  };
   const data = ['Popular', 'Price', 'Alphabet'];
   return (
     <div>
-      <div onClick={() => setIsVisible(!isVisible)}>Sort by:</div>
+      <div onClick={() => setIsVisible(!isVisible)}>Sort by: {data[selected]}</div>
       {isVisible && (
         <div>
           {data.map((item, i) => (
-            <div onClick={() => setSelected(i)} className={selected === i ? 'bg-slate-500' : ''}>
+            <div onClick={() => onClickSetItem(i)} className={selected === i ? 'bg-slate-500' : ''}>
               {item}
             </div>
           ))}

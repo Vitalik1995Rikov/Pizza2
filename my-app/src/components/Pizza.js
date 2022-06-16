fdb3d679ec38bdf6365299271084a43904be14ae
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-06-16 22:15:11 +0300

    added loading

--- my-app/src/components/Pizza.js
+++ my-app/src/components/Pizza.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react';
-// import PizzaItem from './PizzaItem';
+import PizzaItem from './PizzaItem';
 import MyLoader from './MyLoader';
 
 const Pizza = () => {
   const [pizzas, setPizzas] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(
     () =>
       fetch('https://62a8e484943591102bab74e4.mockapi.io/items')
-        .then((res) => res.json())
-        .then((json) => setPizzas(json)),
+        .then((res) => {
+          res.json();
+        })
+        .then((json) => {
+          setPizzas(json);
+          setIsLoading(false);
+        }),
     [],
   );
 
@@ -16,9 +22,9 @@ const Pizza = () => {
     <div className="m-5">
       <span className="text-2xl font-bold">All pizzas</span>
       <ul className="grid grid-cols-4">
-        {pizzas.map((item) => (
-          <MyLoader key={item.id} obj={item} />
-        ))}
+        {isLoading
+          ? [...new Array(6)].map(() => <MyLoader />)
+          : pizzas.map((item) => <PizzaItem key={item.id} obj={item} />)}
       </ul>
     </div>
   );

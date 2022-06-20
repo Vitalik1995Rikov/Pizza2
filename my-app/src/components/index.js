48388cc0e01ef46f482dc6f67064f813ed4b2eac
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-06-20 19:41:22 +0300

    resolve problem

--- my-app/src/components/index.js
+++ my-app/src/components/index.js
@@ -1,6 +1,5 @@
 import Header from './Header';
-import Pizza from './Pizza';
 import Filters from './Filters';
 import Sort from './Sort';
 
-export { Header, Pizza, Filters, Sort };
+export { Header, Filters, Sort };
